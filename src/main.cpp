f090f44575e318b0167c9809768f853efbc21af7
xungeng <xungeng@gmail.com>
2017-11-11 21:13:30 +0800

    sln

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,21 @@
 #include "system.h"
 #include "cpplib.h"
 
-bool error_xx(cpp_reader * rd, int a, int b, rich_location * loc,
-	const char * msg, va_list *v)
+line_maps * g_linetbl = 0;
+
+bool error_xx(cpp_reader * rd, int errtype, int b, rich_location * richloc,
+	const char * msg, va_list *ap)
 {
-	char * msgo = 0;
-	vasprintf(&msgo, msg, *v);
-	fprintf(stderr, "ERROR: %s\n", msgo);
-	free(msgo);
-	abort();
+	const line_map_ordinary *map;
+	source_location location = richloc->get_loc();
+	linemap_resolve_location(g_linetbl, location, LRK_SPELLING_LOCATION, &map);
+	expanded_location loc = linemap_expand_location(g_linetbl, map, location);
+	fprintf(stderr, "%s:%d:%d %s: ", loc.file, loc.line, loc.column,
+		(errtype == CPP_DL_WARNING) ? "warning" : "error");
+	vfprintf(stderr, msg, *ap);
+	fprintf(stderr, "\n");
+	if (errtype == CPP_DL_FATAL)
+		exit(1);
 	return true;
 }
 
@@ -30,6 +37,7 @@ int main(int argc, const char * argv[])
 	linemap_init(line_table, 0);
 	line_table->reallocator = realloc;
 	line_table->round_alloc_size = round_alloc_size;
+	g_linetbl = line_table;
 
 	cpp_reader * rd = cpp_create_reader(CLK_CXX1Z, 0, line_table);
 	printf("%p\n", rd);
